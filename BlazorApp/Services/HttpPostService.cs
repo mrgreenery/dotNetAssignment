c24cac44df7bac334bd71c2fd7f7c7baa95a6bb8
mrgreenery <wilbergroen@gmail.com>
2025-11-13 19:19:43 +0100

    added postservice

--- BlazorApp/Services/HttpPostService.cs
+++ BlazorApp/Services/HttpPostService.cs
@@ -1,6 +1,90 @@
+using System.Text.Json;
+using ApiContract;
+
 namespace BlazorApp.Services;
 
-public class HttpPostService
+public class HttpPostService : IPostService
 {
-    
+    public readonly HttpClient client;
+
+    public HttpPostService(HttpClient client)
+    {
+        this.client = client;
+    }
+    public async Task<PostDto> AddPostAsync(CreatePostDto request)
+    {
+        HttpResponseMessage httpResponse = await client.PostAsJsonAsync("posts", request);
+        string response = await httpResponse.Content.ReadAsStringAsync();
+        if (!httpResponse.IsSuccessStatusCode)
+        {
+            throw new Exception(response);
+        }
+        return JsonSerializer.Deserialize<PostDto>(response, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        })!;
+    }
+
+    public async Task UpdatePostAsync(int id, UpdatePostDto request)
+    {
+        HttpResponseMessage httpResponse = await client.PutAsJsonAsync($"posts/{id}", request);
+        string response = await httpResponse.Content.ReadAsStringAsync();
+        if (!httpResponse.IsSuccessStatusCode)
+        {
+            throw new Exception(response);
+        }
+    }
+
+    public async Task<PostDto> GetPostAsync(int id)
+    {
+        HttpResponseMessage httpResponse = await client.GetAsync($"posts/{id}");
+        string response = await httpResponse.Content.ReadAsStringAsync();
+        if (!httpResponse.IsSuccessStatusCode)
+        {
+            throw new Exception(response);
+        }
+        return JsonSerializer.Deserialize<PostDto>(response, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        })!;
+    }
+
+    public async Task<List<PostDto>> GetPostsAsync(string? title, int? userId)
+    {
+        string url = "posts";
+
+        if (!string.IsNullOrEmpty(title) && userId.HasValue)
+        {
+            url = $"posts?title={title}&userId={userId.Value}";
+        }
+        else if (!string.IsNullOrEmpty(title))
+        {
+            url = $"posts?title={title}";
+        }
+        else if (userId.HasValue)
+        {
+            url = $"posts?userId={userId.Value}";
+        }
+        
+        HttpResponseMessage httpResponse = await client.GetAsync(url);
+        string response =  await httpResponse.Content.ReadAsStringAsync();
+        if (!httpResponse.IsSuccessStatusCode)
+        {
+            throw new Exception(response);
+        }
+        return JsonSerializer.Deserialize<List<PostDto>>(response, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        })!;
+    }
+
+    public async Task DeletePostAsync(int id)
+    {
+        HttpResponseMessage httpResponse = await client.DeleteAsync($"posts/{id}");
+        string response = await httpResponse.Content.ReadAsStringAsync();
+        if (!httpResponse.IsSuccessStatusCode)
+        {
+            throw new Exception(response);
+        }
+    }
 }
--- BlazorApp/Services/IPostService.cs
+++ BlazorApp/Services/IPostService.cs
@@ -1,6 +1,12 @@
+using ApiContract;
+
 namespace BlazorApp.Services;
 
 public interface IPostService
 {
-    
+    public Task<PostDto> AddPostAsync(CreatePostDto request);
+    public Task UpdatePostAsync(int id, UpdatePostDto request);
+    public Task<PostDto> GetPostAsync(int id);
+    public Task<List<PostDto>> GetPostsAsync(string? title, int? userId);
+    public Task DeletePostAsync(int id);
 }
